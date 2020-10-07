c4256a53783c98774bc3769fad9b451ad317dc7d
SergBobrov <serggbobrov@gmail.com>
2020-10-08 01:09:24 +0300

    Updates

---
+++ precache-manifest.b04d645c00a632aabef21eb49fb885d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ddf39bc70b586f6de2ff3081e1e7f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3e46c54b92db6c10bf8",
+    "url": "/static/css/main.71890190.chunk.css"
+  },
+  {
+    "revision": "21360bb8ab0d790ca2fe",
+    "url": "/static/js/2.fefe52f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fefe52f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3e46c54b92db6c10bf8",
+    "url": "/static/js/main.55114d0f.chunk.js"
+  },
+  {
+    "revision": "3b3f68cf2c6891172e85",
+    "url": "/static/js/runtime-main.1c3ce81c.js"
+  }
+]);
